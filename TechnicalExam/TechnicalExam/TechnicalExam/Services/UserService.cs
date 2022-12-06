075813c5465a67fe738505937e0cac6629680e69
mckenley <mckenleylindong@gmail.com>
2022-12-06 22:25:32 +0800

    feat - prepare User Service

---
+++ TechnicalExam/TechnicalExam/TechnicalExam/Services/UserService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TechnicalExam.Models;
+
+namespace TechnicalExam.Services
+{
+    public class UserService : IUserService
+    {
+        public Task<List<UserModel>> GetUsers()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
